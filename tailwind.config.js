8122f5aff3b674d3b58a48195947579b934f5acb
zouhair <zouhairboudeir768@gmail.com>
2026-02-04 01:24:28 +0100

    Fix build errors: add missing dependencies and fix configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/preline/preline.js",
   ],
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require("preline/plugin")],
+  plugins: [],
 };
